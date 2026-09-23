a30cf3c0da79a3b851bc2a07883b9dc8f50549bc
Dobi04 <nj675896@gmail.com>
2026-09-23 13:19:03 +0200

    -Added CurenUserService (will be used for Auth/me)

---
+++ backend/src/ExcursionSaaS.API/Services/CurrentUserService.cs
@@ -0,0 +1,28 @@
+﻿using ExcursionSaaS.Application.Services;
+using System.Security.Claims;
+
+namespace ExcursionSaaS.API.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            this._httpContextAccessor = httpContextAccessor;
+        }
+
+        public int? UserId
+        {
+            get
+            {
+                var value = _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                return int.TryParse(value, out var userId) ? userId : null;
+            }
+        }
+
+        public string? Username => _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.Name);
+
+        public string? IpAddress => _httpContextAccessor.HttpContext?.Connection.RemoteIpAddress?.ToString();
+    }
+}
---
+++ backend/src/ExcursionSaaS.Application/Services/ICurrentUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcursionSaaS.Application.Services
+{
+    public interface ICurrentUserService
+    {
+        int? UserId { get; }
+        string? Username { get; }
+        string? IpAddress { get; }
+    }
+}
